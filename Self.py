e03e6b363750a2896c0163d7058c352e5b37da5e
Matt Arnold <matt.arnold44@gmail.com>
2014-03-01 01:10:31 -0500

    should handle bad input now

--- Self.py
+++ Self.py
@@ -6,9 +6,9 @@ class Self (object):
     def __init__(self, was=None, am=None, want2be=None): # add parameters
         self.was = was
         self.am = am
-        self.want2be = want2be  # should it be 'other' or 'want2be'?
+        self.want2be = want2be  
         self.zen = Trait('no-mind')  # create private zen object
-        self.Kierk = Trait('Soren Kierkegaard (Knight of Faith)')
+        self.Kierk = Trait('Soren Kierkegaard (Knight of Faith)') # create private Kierkegaard object
         self.hemi = None
 
     def isEast(self):
@@ -59,7 +59,7 @@ class Self (object):
         self.was.append(self.am)
         self.am = self.Kierk
         self.want2be = None
-        print '\n\n\t you became Soren Kierkegaard\n'
+        print '\n\n\t you became S0ren Kierkegaard\n'
          
     def getWas(self):
         return str(self.was)
@@ -91,20 +91,16 @@ class Trait (object):
         self.adj = newAdj
 
 # ===========================================================
-
-def askBe():
-    a = raw_input('do you want to be? [y/n/quit]\n')
-    print a
-    return a
+# other functions:
 
 def printOptions():
     print '\nOPTIONS:'
     print '\t1. i am...'
     print '\t2. become [x]'
+    print '\t[q] to quit'
 
 def getInput(prompt):
     o = raw_input(prompt)
-    print o
     return o
 
 def setEastWest():
@@ -120,48 +116,67 @@ def setEastWest():
             EastWest = False
 
 ###################################################
-print '\nnew game\n\n'    
-quit = False
-while quit != True:
-    beAnsw = askBe()
+#             MAIN                                #
+###################################################
 
-    if beAnsw == "n":
-        # never existed
-        print '\nunborn\n'
+if __name__ == "__main__":
+    print '\nnew game\n\n'    
+    quit = False
+    while quit != True:
+        beAnsw = getInput('do you want to be? [y/n/quit]\n')
 
-    elif beAnsw == "y":
-        # choosing to exist
-        i = Self()
-        print '\nyou exist, as an object'
+        if beAnsw == "n":
+            # never existed
+            print '\nunborn\n'
+
+        elif beAnsw == "y":
+            # choosing to exist
+            i = Self()
+            print '\nyou exist, as an object'
+            
+            setEastWest()
+
+            quit = False
+            while quit != True:
+                # the print options loop
+                printOptions()
+                choice = getInput('choose\n')
         
-        setEastWest()
+                if choice == '1':
+                    i.printMe()
+                
+                elif choice == '2': 
+                    
+                    while True:
+                        want = getInput('what do you want to become?\n\t1. what i am\n\t2. something else...\n')
+                        if want == '1':
+                            if i.getHemi() == 'east':
+                                i.goZen()
+                            else:
+                                i.goKierk()
+                            break
+                        elif want == '2':
+                            want2be = getInput('choose anything\n')
+                            print '\ninteresting choice'
+                             
+                            while True:
+                                response = getInput('how bad do you want it?\n\t1. bad\n\t2. real bad\n\t3. i don\'t\n')
+                                if response == '1' or response == '2':
+                                    i.plantDesire(want2be)
+                                    break
+                                elif response == '3':
+                                    i.actualize(want2be)
+                                    break
+                                else:
+                                    print '\ninvalid choice\n'
+                            break    
+                        else:
+                            print '\ninvalid choice\n'
 
-        quit = False
-        while quit != True:
-            # the print options loop
-            printOptions()
-            choice = int(getInput('choose\n'))
-    
-            if choice == 1:
-                i.printMe()
-            elif choice == 2:
-                want = int(getInput('what do you want to become?\n\t1. what i am\n\t2. something else...\n'))
+                elif choice == 'q':
+                    quit = True
+                else:
+                    print '\ninvalid choice'
         
-                if want == 1:
-                    if i.getHemi() == 'east':
-                        i.goZen()
-                    else:
-                        i.goKierk()
-                elif want == 2:
-                    want2be = getInput('choose anything\n')
-                    print '\ninteresting choice'
-                    response = int(getInput('how bad do you want it?\n\t1. bad\n\t2. real bad\n\t3. i don\'t\n'))
-                    if response == 1 or response == 2:
-                        i.plantDesire(want2be)
-                    elif response == 3:
-                        i.actualize(want2be)
-    
-    elif beAnsw == 'quit':
-        quit = True
-
-    
+        elif beAnsw == 'quit':
+            quit = True 
